9e5bf1b9e52912f9947ba025fa0de06ef1871d0a
ajalasegun1 <ajalasegun1@gmail.com>
2021-04-27 17:17:59 +0100

    categpry crud done

--- controllers/category.js
+++ controllers/category.js
@@ -5,3 +5,39 @@ exports.create = (req, res) => {
     .then((data) => res.json(data))
     .catch((err) => res.json(err));
 };
+exports.read = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.update = (req, res) => {
+  Category.findByIdAndUpdate(req.category._id, req.body)
+    .then((category) => {
+      return res.json({ message: `Category  ${category._id} updated` });
+    })
+    .catch(() => {
+      return res.json({ error: "Something went wrong updating the category" });
+    });
+};
+
+exports.remove = (req, res) => {
+  Category.findByIdAndDelete(req.category._id)
+    .then((deleted) => {
+      return res.json({ message: `${deleted._id} has been deleted...` });
+    })
+    .catch(() => {
+      return res.json({
+        error: "Something went wrong while trying to delete catgory...",
+      });
+    });
+};
+
+exports.categoryById = (req, res, next, id) => {
+  Category.findById(id)
+    .then((category) => {
+      req.category = category;
+      next();
+    })
+    .catch(() => {
+      return res.json({ error: "Category not found..." });
+    });
+};
--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { create } = require("../controllers/category");
-const {userById} = require("../controllers/user")
-const {requireSignin, isAuth, isAdmin} = require("../controllers/auth")
+const {
+  create,
+  categoryById,
+  read,
+  update,
+  remove
+} = require("../controllers/category");
+const { userById } = require("../controllers/user");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
 router.post("/create/:userID", requireSignin, isAuth, isAdmin, create);
-router.param("userID", userById)
+router.get("/:categoryID", read);
+router.put("/:categoryID/:userID", requireSignin, isAuth, isAdmin, update);
+router.delete("/:categoryID/:userID", requireSignin, isAuth, isAdmin, remove);
+router.param("userID", userById);
+router.param("categoryID", categoryById);
 
 module.exports = router;
